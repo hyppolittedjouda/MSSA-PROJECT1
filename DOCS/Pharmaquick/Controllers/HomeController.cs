fa53183bef524998f8fdb8f7c7689cc15f596606
Hyppolitte-Djouda <hyppolitte.djouda@outlook.com>
2019-11-05 19:32:03 -0500

    Using ASP.NET Core MVC to build Phamaquick web app

---
+++ DOCS/Pharmaquick/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Pharmaquick.Models;
+
+namespace Pharmaquick.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Patient()
+        {
+            return View();
+        }
+
+        public IActionResult Provider()
+        {
+            return View();
+        }
+
+        public IActionResult Medication()
+        {
+            return View();
+        }
+
+        public IActionResult Allergy()
+        {
+            return View();
+        }
+
+        public IActionResult History()
+        {
+            return View();
+        }
+
+        public IActionResult Immunization()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
